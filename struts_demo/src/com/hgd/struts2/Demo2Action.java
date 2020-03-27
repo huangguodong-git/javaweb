7c55fdcc18a80d1b32459f6eeee646bf7acf7eb4
haungguodong-git <huangguodong_hgd@163.com>
2020-03-27 16:57:09 +0800

    struts2的入门代码提交

---
+++ struts_demo/src/com/hgd/struts2/Demo2Action.java
@@ -0,0 +1,17 @@
+package com.hgd.struts2;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ * @auther 黄国栋
+ * @data 2020-03-27 14:27
+ * @since
+ */
+public class Demo2Action implements Action {
+
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+}
---
+++ struts_demo/src/com/hgd/struts2/Demo3Action.java
@@ -0,0 +1,17 @@
+package com.hgd.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @auther 黄国栋
+ * @data 2020-03-27 14:29
+ * @since
+ */
+public class Demo3Action extends ActionSupport {
+
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+}
