38d0e64e726585ec6e9a8aa8ad717ddea2dcd075
haungguodong-git <huangguodong_hgd@163.com>
2020-03-27 16:57:57 +0800

    struts2的入门代码提交

---
+++ struts_hello/src/com/hgd/struts2/HelloAction.java
@@ -0,0 +1,15 @@
+package com.hgd.struts2;
+
+/**
+ * @auther 黄国栋
+ * @data 2020-03-27 09:11
+ * @since
+ */
+public class HelloAction {
+
+    public String hello(){
+        System.out.println("struts2的入门程序。。。");
+        return "success";
+    }
+
+}
