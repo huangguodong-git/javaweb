3fe692a6c11a6de8720c866af015572931c8c7d1
haungguodong-git <huangguodong_hgd@163.com>
2020-04-02 15:04:54 +0800

    餐厅管理后台系统

--- hotel/src/com/hgd/hotel/controller/IndexController.java
+++ hotel/src/com/hgd/hotel/controller/IndexController.java
@@ -1,19 +1,21 @@
 package com.hgd.hotel.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(name = "/sys/index.action")
+/**
+ * 
+ */
+@WebServlet("/sys/index.action")
 public class IndexController extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
+	private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
-    }
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/sys/index.jsp").forward(request, response);
+	}
+
 }
