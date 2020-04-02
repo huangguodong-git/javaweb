5e0fcd8cf57d9691c9128ac366c493247eb69122
haungguodong-git <huangguodong_hgd@163.com>
2020-04-03 06:47:08 +0800

    菜系查询

--- hotel/src/com/hgd/hotel/controller/FoodController.java
+++ hotel/src/com/hgd/hotel/controller/FoodController.java
@@ -17,19 +17,9 @@ import com.hgd.hotel.service.impl.FoodTypeServiceImpl;
 @WebServlet("/sys/food.action")
 public class FoodController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	private FoodTypeService foodTypeService=new FoodTypeServiceImpl();
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String method=request.getParameter("method");
-		if (method!=null && method.equals("list")) {
-			List<FoodType> foodTypes=foodTypeService.findAll();
-			for (FoodType foodType : foodTypes) {
-				System.out.println(foodType);
-			}
-			request.setAttribute("foodTypes",foodTypes);
-			request.getRequestDispatcher("/WEB-INF/jsp/sys/foodList.jsp").forward(request, response);
-		}
+		request.getRequestDispatcher("/WEB-INF/jsp/sys/foodList.jsp").forward(request, response);
 	}
 
 }
